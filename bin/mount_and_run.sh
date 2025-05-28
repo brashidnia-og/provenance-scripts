7fcbf021e9a410895632ab3fc7c469c1a498dc43
Bobak <brashidnia@gmail.com>
2025-05-28 05:07:24 -0700

    WIP bash scripts to setup a provenance node on a EC2 Ubuntu host

---
+++ bin/mount_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+current_dir=`dirname $0`
+echo "Running from ${current_dir}"
+
+script_dir="${current_dir}/mount_and_run"
+echo "Running scripts in ${script_dir}"
+
+sudo chmod 777 ${script_dir}/*.sh
+
+mount_disk_path="${script_dir}/1_mount_disk.sh"
+echo "Running ${mount_disk_path}"
+sudo ${mount_disk_path}
+
+run_node_path="${script_dir}/2_run_node.sh"
+echo "Running ${run_node_path}"
+sudo ${run_node_path}
---
+++ bin/setup_node_ebs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+current_dir=`dirname $0`
+echo "Running from ${current_dir}"
+
+script_dir="${current_dir}/setup_node_ebs"
+echo "Running scripts in ${script_dir}"
+
+sudo chmod 777 ${script_dir}/*.sh
+
+#format_ebs_partition_path="${script_dir}/1_format_partition.sh"
+#echo "Running ${format_ebs_partition_path}"
+#sudo ${format_ebs_partition_path}
+#
+#mount_disk_script_path="${script_dir}/2_mount_disk.sh"
+#echo "Running ${mount_disk_script_path}"
+#sudo ${mount_disk_script_path}
