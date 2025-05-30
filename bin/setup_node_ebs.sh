bdeaa7d1b24ec616d9f61efff999679c956b0a1f
Bobak <brashidnia@gmail.com>
2025-05-29 23:05:10 -0700

    fix call

--- bin/setup_node_ebs.sh
+++ bin/setup_node_ebs.sh
@@ -12,8 +12,8 @@ sudo chmod 777 ${script_dir}/*.sh
 
 format_ebs_partition_path="${script_dir}/1_format_partition.sh"
 echo "Running ${format_ebs_partition_path}"
-sudo ${format_ebs_partition_path}
+${format_ebs_partition_path}
 
 mount_disk_script_path="${script_dir}/2_mount_disk.sh"
 echo "Running ${mount_disk_script_path}"
-sudo ${mount_disk_script_path}
+${mount_disk_script_path}
